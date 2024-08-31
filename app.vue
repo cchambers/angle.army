bdfb07ffb5f3197ef58d6c916525369869460bdf
Chris Chambers <cchambers@gmail.com>
2024-08-31 10:25:36 -0400

    fix

--- app.vue
+++ app.vue
@@ -12,3 +12,13 @@ onMounted(() => {
     <!-- Logo or something -->
   </div>
 </template>
+
+<style lang="scss">
+body {
+  padding-top: 30%;
+  background: #222;
+  color: #f1f1f1;
+  text-align: center;
+  font-family: sans-serif;
+}
+</style>
